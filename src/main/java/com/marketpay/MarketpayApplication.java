9b682b89a3405f95741d922f182de698a318da9d
Lucas Mega <lucas.mega07@gmail.com>
2021-05-02 20:38:37 -0300

    Primeiro commit do projeto

---
+++ src/main/java/com/marketpay/MarketpayApplication.java
@@ -0,0 +1,13 @@
+package com.marketpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marketpay/MarketpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
